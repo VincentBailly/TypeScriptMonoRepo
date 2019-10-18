c531a575f64be9cc9cedda08f66e2ed496c3ee99
Vincent <Vincent.Bailly@microsoft.com>
2019-10-18 08:34:10 +0100

    remove tsconfig.release

--- packages/A/webpack.config.js
+++ packages/A/webpack.config.js
@@ -3,13 +3,7 @@ const path = require("path");
 config = {
   mode: "development",
   entry: "./src/index.ts",
-  // We use inline source-map to enable sub-line breakpoints,
-  // if perf turns out to be a problem we can experiment less accurate and faster
-  // mapping strategies.
-  // Note that we only map to .js files and not to .ts files, the reasons is that
-  // the debugger experience with .ts mapping does not work as expected and the
-  // .js files are very close to the .ts files so they should be easy to debug.
-  devtool: "inline-source-map",
+  devtool: "cheap-module-eval-source-map",
   context: __dirname,
   devServer: {
     contentBase: "./dist",
@@ -21,11 +15,6 @@ config = {
         test: /\.ts$/,
         use: [{ loader: "babel-loader", options: { presets: ["@babel/preset-typescript"], cacheDirectory: true, cacheCompression: false }}],
         exclude: /.*\/node_modules\/.*/
-      },
-      {
-        test: /\.js$/,
-        use: ["source-map-loader"],
-        enforce: "pre"
       }
     ]
   },
